6889cbd057497bf66b0d0215aeec43f112152fcd
JHANANI27 <108513771+JHANANI27@users.noreply.github.com>
2023-03-31 12:17:59 +0530

    Add files via upload

---
+++ Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../pages/Contact.css"
+
+export default function Contact() {
+  return (
+    <section id='header4'>
+        <div>
+            <h1><u>Contact</u></h1>
+        </div>
+       <div className='foot'>
+          <>
+          <p><b>Phone no : </b>8870270333</p>
+        <p><b>Contact address :</b> COIMBATORE FUNMALL</p>
+        <p>Coimbatore,Tamil Nadu</p>
+          </>
+       </div>
+    </section>
+  )
+}
